dd31a6bbfb3432d743c1bef8c5d2d33164572854
James N. V. Cash <james.nvc@gmail.com>
2012-04-16 18:15:19 -0400

    Adding more .js stuff

--- dotjs/achewood.com.js
+++ dotjs/achewood.com.js
@@ -1,2 +1,3 @@
+// jQuery is included in dotjs - can we use that to streamline this?
 var bdy = document.getElementById("comic_body");
 bdy.appendChild(document.createTextNode(bdy.getElementsByTagName("img")[0].title));
