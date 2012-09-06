af976db46527039ade337704e542267ebdf2ea56
James N. V. Cash <james.nvc@gmail.com>
2012-09-06 17:14:09 -0400

    Enhancing contact searching script

--- mutt/search_contacts.py
+++ mutt/search_contacts.py
@@ -8,16 +8,22 @@ CONTACTS_FORMAT_STR = '{}TOKEN%n'
 def search_for(term):
     results = set()
     for email_tok in ['%e', '%we', '%oe']:
-        output = subprocess.check_output([
-            'contacts', '-HSf', CONTACTS_FORMAT_STR.format(email_tok), term
-        ])
-        addrs = [addr.replace('TOKEN', '\t') for addr in output.split('\n')
-                 if addr and addr.find('@') != -1]
-        results |= set(addrs)
+        try:
+            output = subprocess.check_output([
+                'contacts', '-HSf', CONTACTS_FORMAT_STR.format(email_tok), term
+            ])
+            addrs = [addr.replace('TOKEN', '\t') for addr in output.split('\n')
+                    if addr and addr.find('@') != -1]
+            results |= set(addrs)
+        except subprocess.CalledProcessError:
+            pass
     return results
 
 if __name__ == '__main__':
     import sys
+    if len(sys.argv) < 2:
+        sys.exit(0)
     addrs = search_for(sys.argv[1])
-    print 'EMAIL\tNAME'
-    print '\n'.join(addrs)
+    if len(addrs):
+        print 'EMAIL\tNAME'
+        print '\n'.join(addrs)
