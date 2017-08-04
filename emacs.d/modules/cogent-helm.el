1bc487f1a7433b88606080bac8864d06acaeb113
James N. V. Cash <james.nvc@gmail.com>
2017-08-04 12:09:38 -0400

    Formatting + binding for line search

--- emacs.d/modules/cogent-helm.el
+++ emacs.d/modules/cogent-helm.el
@@ -11,21 +11,21 @@
       :config (helm-projectile-on)))
   (helm-autoresize-mode 1)
   (setq-default helm-display-header-line nil
-		helm-autoresize-min-height 0
-		helm-autoresize-max-height 35
-		helm-split-window-in-side-p t
+                helm-autoresize-min-height 0
+                helm-autoresize-max-height 35
+                helm-split-window-in-side-p t
 
-		helm-M-x-fuzzy-match t
-		helm-buffers-fuzzy-matching t
-		helm-recentf-fuzzy-match t
-		helm-apropos-fuzzy-match t)
+                helm-M-x-fuzzy-match t
+                helm-buffers-fuzzy-matching t
+                helm-recentf-fuzzy-match t
+                helm-apropos-fuzzy-match t)
   (set-face-attribute 'helm-source-header nil :height 0.75)
   :bind (("M-x" . helm-M-x)
-	 ("C-x C-f" . helm-find-files)
-	 ("C-x C-g" . helm-do-grep)
-	 ("C-x b" . helm-buffers-list)
-	 ("C-t" . helm-imenu)
-	 ("M-y" . helm-show-kill-ring)))
+         ("C-x C-f" . helm-find-files)
+         ("C-x C-g" . helm-do-grep)
+         ("C-x b" . helm-buffers-list)
+         ("C-t" . helm-imenu)
+         ("M-y" . helm-show-kill-ring)))
 
 (use-package swiper-helm
   :bind (("C-S-s" . swiper-helm)))
