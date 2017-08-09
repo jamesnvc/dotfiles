aa781f1ba238ba6f64587f31f49ec69b94c841c0
James N. V. Cash <james.nvc@gmail.com>
2017-08-09 13:46:26 -0400

    Add prettify-symbol mode hook for emacs lisp mode

--- emacs.d/user.el
+++ emacs.d/user.el
@@ -121,9 +121,9 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (global-set-key (kbd "<f3>") 'eshell)
 
 ;; Fancy symbols
-(prettify-symbols-mode 1)
 (setq prettify-symbols-alist
       '(("lambda" . 955)))
+(add-hook 'emacs-lisp-mode-hook 'prettify-symbols-mode)
 
 (with-eval-after-load "company"
   (evil-define-key 'insert company-active-map (kbd "C-w") #'evil-delete-backward-word)
