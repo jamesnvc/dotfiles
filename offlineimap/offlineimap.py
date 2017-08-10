7cf44a96eabb6db46d7fa401ce510e836f7530aa
James N. V. Cash <james.nvc@gmail.com>
2017-08-10 15:25:27 -0400

    Add docs to offlineimap python script

--- offlineimap/offlineimap.py
+++ offlineimap/offlineimap.py
@@ -1,3 +1,13 @@
+"""Script to use in offlineimap to read encrypted mail passwords
+
+To use, include the lines `pythonfile = <path to this file>` under the [general]
+section and `remotepasseval = mailpassword("account-name")` under the remote
+repository section of your offlineimaprc.
+
+To create the encrypted files, create a file in
+`~/.passwd/account-name` containing your password, then run
+`gpg -e -r $gpg_key_id ~/.password/account-name` then delete the unencrypted file
+"""
 import os
 import subprocess
 
