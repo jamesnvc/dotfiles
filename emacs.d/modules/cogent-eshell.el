5fbbdae776541860f5a5073e5c6068163efcdf09
James N. V. Cash <james.nvc@gmail.com>
2017-09-08 12:11:13 -0400

    Use helm for eshell history, don't set global key in module
    Let the global binding to switch to/open an eshell be set in user.el

--- emacs.d/modules/cogent-eshell.el
+++ emacs.d/modules/cogent-eshell.el
@@ -1,9 +1,11 @@
 ;;; -*- lexical-binding: t -*-
 
-(global-set-key (kbd "<f3>") 'eshell)
-
 (use-package eshell-git-prompt
   :config
   (eshell-git-prompt-use-theme 'powerline))
+
+(add-hook 'eshell-mode-hook
+          (lambda () (define-key eshell-mode-map (kbd "M-r")
+                  #'helm-eshell-history)))
 
 (provide 'cogent-eshell)
