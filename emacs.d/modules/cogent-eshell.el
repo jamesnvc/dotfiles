dd2f674e5f0769333c5e4f2225121f5de93e6484
James N. V. Cash <james.nvc@gmail.com>
2017-09-08 19:21:50 -0400

    Move eshell conf hook to user

--- emacs.d/modules/cogent-eshell.el
+++ emacs.d/modules/cogent-eshell.el
@@ -4,8 +4,4 @@
   :config
   (eshell-git-prompt-use-theme 'powerline))
 
-(add-hook 'eshell-mode-hook
-          (lambda () (define-key eshell-mode-map (kbd "M-r")
-                  #'helm-eshell-history)))
-
 (provide 'cogent-eshell)
