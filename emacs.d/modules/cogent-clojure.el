3d2f320a25045677b4e96146caff8ca3076636bb
James N. V. Cash <james.nvc@gmail.com>
2017-11-21 20:53:31 -0500

    Clean up cogent-clojure, use general for keybindings

--- emacs.d/modules/cogent-clojure.el
+++ emacs.d/modules/cogent-clojure.el
@@ -4,31 +4,31 @@
 (use-package clojure-mode
   :commands clojure-mode
   :config
-  (add-hook
-   'clojure-mode-hook
-   (lambda ()
-     (enable-paredit-mode)
-     (use-package cider
-       :commands cider-jack-in
-       :config
-       (add-hook 'cider-mode-hook 'eldoc-mode)
-       (setq cider-prompt-for-symbol nil)
-       (setq cider-font-lock-dynamically '(macro core function var))
-       (with-eval-after-load "helm"
-         (use-package helm-cider
-           :config (helm-cider-mode 1))))
-     (use-package clj-refactor
-       :config
-       (add-hook 'clojure-mode-hook (lambda () (clj-refactor-mode 1)))
-       (use-package cljr-helm
-         :commands cljr-helm
-         :config
-         (general-nmap :prefix "SPC"
-                       :keymaps 'clojure-mode-map
-                       "r" 'cljr-helm)))))
-  (with-eval-after-load "company"
-    (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
-    (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)))
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+
+  (use-package cider
+    :commands cider-jack-in
+    :config
+    (add-hook 'cider-mode-hook 'eldoc-mode)
+    (evil-set-initial-state 'cider-docview-mode 'insert)
+    (evil-set-initial-state 'cider-stacktrace-mode 'insert)
+    (setq cider-prompt-for-symbol nil)
+    (setq cider-font-lock-dynamically '(macro core function var))
+    (with-eval-after-load "company"
+      (add-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+      (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion))
+    (with-eval-after-load "helm"
+      (use-package helm-cider
+        :config (helm-cider-mode 1))))
+
+  (use-package clj-refactor
+    :config
+    (add-hook 'clojure-mode-hook (lambda () (clj-refactor-mode 1))))
+
+  (use-package cljr-helm
+    :general
+    (:keymaps 'clojure-mode-map :states '(normal)
+              "<SPC>r" 'cljr-helm)))
 
 (use-package paredit
   :commands enable-paredit-mode paredit-mode
