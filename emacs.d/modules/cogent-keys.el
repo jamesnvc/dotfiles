a3fce2c1f511768e6c5da19e1cd50a2462a7f725
James N. V. Cash <james.nvc@gmail.com>
2017-11-21 19:00:16 -0500

    Default general bindings to normal & visual mode

--- emacs.d/modules/cogent-keys.el
+++ emacs.d/modules/cogent-keys.el
@@ -3,7 +3,7 @@
 (use-package general
   :demand t
   :config
-  (setq general-default-keymaps 'evil-normal-state-map)
+  (setq general-default-keymaps '(evil-normal-state-map evil-visual-state-map))
   ;; (setq general-default-prefix "SPC")
   (general-evil-setup))
 
