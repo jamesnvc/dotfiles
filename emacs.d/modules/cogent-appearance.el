64daa7200c5db042ccb3161512453710dd8d35e7
James N. V. Cash <james.nvc@gmail.com>
2017-11-23 14:32:28 -0500

    Show git-gutter mode with git icon

--- emacs.d/modules/cogent-appearance.el
+++ emacs.d/modules/cogent-appearance.el
@@ -84,7 +84,7 @@
 (eval-after-load "smartparens" '(diminish 'smartparens-mode))
 (eval-after-load "undo-tree" '(diminish 'undo-tree-mode))
 (eval-after-load "flycheck" '(diminish 'flycheck-mode))
-(eval-after-load "git-gutter+" '(diminish 'git-gutter+-mode))
+(eval-after-load "git-gutter+" '(diminish 'git-gutter+-mode ""))
 (eval-after-load "evil-mc" '(diminish 'evil-mc-mode))
 (eval-after-load "geiser" '(diminish 'geiser-autodoc-mode))
 (eval-after-load "company" '(diminish 'company-mode))
