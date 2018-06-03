ebb9b6d2b9718c0aafa0a6dab26df09f27b0b3c0
James N. V. Cash <james.nvc@gmail.com>
2018-06-03 19:22:50 -0400

    Add eshell glob thing to search for org tags

--- emacs.d/modules/cogent-eshell.el
+++ emacs.d/modules/cogent-eshell.el
@@ -4,4 +4,23 @@
   :config
   (eshell-git-prompt-use-theme 'powerline))
 
+;; Make T function as an eshell glob to find org files with a matching tag
+;; e.g. `ls *.org(T'git')`'
+(defun eshell-org-file-tags ()
+  "Helps the eshell parse the text the point is currently on,
+looking for parameters surrounded in single quotes. Returns a
+function that takes a FILE and returns nil if the file given to
+it doesn't contain the org-mode #+TAGS: entry specified."
+  (if (looking-at "'\\([^)']+\\)'")
+      (let* ((tag (match-string 1))
+             (reg (concat "^#\\+TAGS: .*" tag "\\b")))
+        (goto-char (match-end 0))
+        `(lambda (file)
+           (with-temp-buffer
+             (insert-file-contents file)
+             (re-search-forward ,reg nil t 1))))
+    (error "The `T' predicate takes an org-mode tag value in single quotes.")))
+(add-hook 'eshell-pred-load-hook (lambda ()
+  (add-to-list 'eshell-predicate-alist '(?T . (eshell-org-file-tags)))))
+
 (provide 'cogent-eshell)
