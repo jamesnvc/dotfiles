acb01b00f4e5d5eb606f0f915410bf0c62036a13
James N. V. Cash <james.nvc@gmail.com>
2018-10-04 17:47:15 -0400

    Don't switch to process buffer on load

--- emacs.d/modules/prolog.el
+++ emacs.d/modules/prolog.el
@@ -1491,8 +1491,7 @@ If COMPILEP is non-nil then use compilation, otherwise consulting."
     (process-send-string
      "prolog" (prolog-build-prolog-command
              compilep buffer-file-name
-             (prolog-bsts buffer-file-name)))
-  (prolog-goto-prolog-process-buffer))
+             (prolog-bsts buffer-file-name))))
 
 
 ;;------------------------------------------------------------
