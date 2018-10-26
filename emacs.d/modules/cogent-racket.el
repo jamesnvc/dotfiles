238a42d16c70f196f3952e6654cdf44feec61f84
James N. V. Cash <james.nvc@gmail.com>
2018-10-26 12:58:59 -0400

    Use racket-mode instead of quack

--- emacs.d/modules/cogent-racket.el
+++ emacs.d/modules/cogent-racket.el
@@ -1,13 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 (require 'cogent-package)
 
-(use-package geiser
-  :commands run-geiser
-  :init
-  (use-package quack
-    :demand t)
-  (add-hook 'scheme-mode-hook #'enable-paredit-mode)
-  (evil-define-key 'normal geiser-mode-map
-    (kbd "] C-d") #'geiser-edit-symbol-at-point))
+(use-package racket-mode)
 
 (provide 'cogent-racket)
