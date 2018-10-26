b3706b119584e551e2b9a511c63f310f56830151
James N. V. Cash <james.nvc@gmail.com>
2018-10-26 19:15:49 -0400

    Fix setup & hooks for emacs language servers

--- emacs.d/modules/cogent-lsp.el
+++ emacs.d/modules/cogent-lsp.el
@@ -3,8 +3,8 @@
 (require 'cogent-package)
 
 (use-package lsp-mode
-  :commands lsp-define-stdio-client lsp-mode
-  :init
+  :commands lsp-define-stdio-client lsp-mode lsp-sh-enable
+  :config
   (require 'lsp-imenu)
   (add-hook 'lsp-after-open-hook #'lsp-enable-imenu)
   (use-package lsp-ui
@@ -15,28 +15,25 @@
     :demand t
     :init (push 'company-lsp company-backends))
 
-  :config
-  ;; backends
-
   ;; SH
   ;; to install the client:
   ;; npm i -g bash-language-server
   (lsp-define-stdio-client
    lsp-sh "sh"
    #'(lambda () default-directory)
-   `(,(let ((npm-prefix (s-chomp (shell-command-to-string "npm config get prefix")))
-            )
+   `(,(let ((npm-prefix (s-chomp (shell-command-to-string "npm config get prefix"))))
         (s-append "/bin/bash-language-server" npm-prefix))
      "start"))
-  (add-hook 'sh-mode #'lsp-sh-enable)
+  ;; not enabling it by default - kinda overkill
+  ;; (add-hook 'sh-mode-hook #'lsp-sh-enable)
 
-
+  :init
   ;; Rust
   ;; to install the client:
   ;; rustup component add rls-preview rust-analysis rust-src
   (use-package lsp-rust
     :commands lsp-rust-enable
-    :init (add-hook 'rust-mode #'lsp-rust-enable))
-  )
+    :init
+    (add-hook 'rust-mode-hook #'lsp-rust-enable)))
 
 (provide 'cogent-lsp)
