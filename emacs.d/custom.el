d445dfada3367e9cca9190c40cf0222342564a63
James N. V. Cash <james.nvc@gmail.com>
2018-11-11 11:13:25 -0500

    Trying to make themes not mess with custom-set-faces

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -132,5 +132,4 @@ Entered on %U
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((min-colors 16777216)) (:background "#282a36" :foreground "#f8f8f2")) (t (:background "#000000" :foreground "#f8f8f2"))))
  '(spaceline-evil-visual ((t (:inherit mode-line :background "dark slate blue" :foreground "#ffffff")))))
