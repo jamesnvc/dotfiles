8bfc30e07937f5dbf1553ecf3565aa28fb98d4a8
James N. V. Cash <james.nvc@gmail.com>
2018-11-27 16:02:33 -0500

    Add custom settings for display-line-numbers
    I thought I had cherry-picked this from master already, but apparently not

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -12,6 +12,7 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" default)))
+ '(display-line-numbers-type (quote relative))
  '(elfeed-search-face-alist
    (quote
     ((unread elfeed-search-unread-title-face)
