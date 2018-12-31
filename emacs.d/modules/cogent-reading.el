1fe06efc7a74b2b051e883809ea026c4e96cf94c
James N. V. Cash <james.nvc@gmail.com>
2018-12-31 18:28:57 -0500

    Fix hook for pdf-tools view mode

--- emacs.d/modules/cogent-reading.el
+++ emacs.d/modules/cogent-reading.el
@@ -2,14 +2,14 @@
 
 (require 'cogent-package)
 
-(defun cogent/display-display-line-numbers-mode ()
+(defun cogent/disable-display-line-numbers-mode ()
   (display-line-numbers-mode -1))
 
 (use-package pdf-tools
   :demand t
   :config
   (pdf-tools-install)
-  (add-hook 'pdf-view-mode-hook #'display-line-numbers-mode))
+  (add-hook 'pdf-view-mode-hook #'cogent/disable-display-line-numbers-mode))
 
 
 (provide 'cogent-reading)
