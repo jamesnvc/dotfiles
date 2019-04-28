75b8b272efa88f07aca77bdfbe2f89dd416c1b3f
James N. V. Cash <james.nvc@gmail.com>
2019-04-28 15:38:02 -0400

    Fix eshell switching helm thing to prefer existing dir

--- emacs.d/modules/cogent-eshell-helm.el
+++ emacs.d/modules/cogent-eshell-helm.el
@@ -4,56 +4,57 @@
 (require 'helm-lib)
 (require 'cl-lib)
 
-(defun cogent/eshell-buffers ()
-  (cl-loop for buf in (buffer-list)
-           when (string-prefix-p "*eshell*" (buffer-name buf))
-           collect buf))
+(defun cogent/eshell-helm--get-candidates ()
+  (let* ((eshells (cl-loop for buf in (buffer-list)
+                           when (string-prefix-p "*eshell*" (buffer-name buf))
+                           collect (cons (pwd-replace-home
+                                          (buffer-local-value
+                                           'default-directory buf))
+                                         buf)))
+         (new-dir (if (string-blank-p helm-input)
+                      default-directory
+                    helm-input))
+         (new-eshell (cons (concat
+                            (propertize
+                             " " 'display
+                             (propertize "[+]" 'font-lock-face
+                                         '(:background "#ff69c6" :foreground "#282a36")))
+                            " "
+                            (pwd-replace-home new-dir))
+                           new-dir)))
+    (cons new-eshell eshells)))
+
+(defun cogent/eshell-helm-move-to-first-real-candidate ()
+  (let ((sel (helm-get-selection nil nil (helm-get-current-source))))
+    (unless (bufferp sel)
+      (helm-next-line))))
 
 ;;;###autoload
 (defun cogent/eshell-helm ()
   "Switch between or create eshell buffers using helm"
   (interactive)
-  (helm :sources
-        (helm-build-sync-source "eshell"
-          :candidates
-          (lambda ()
-            (->>
-             (cl-loop for buf in (cogent/eshell-buffers)
-                      collect (cons (pwd-replace-home
-                                     (buffer-local-value
-                                      'default-directory buf))
-                                    buf))
-             (cons
-              (let ((new-dir (if (string-blank-p helm-input)
-                                 default-directory
-                               helm-input)))
-                (cons (concat
-                       (propertize
-                        " " 'display
-                        (propertize "[+]" 'font-lock-face
-                                    '(:background "#ff69c6" :foreground "#282a36")))
-                       " "
-                       (pwd-replace-home new-dir))
-                      new-dir)))))
-          :action (list
-                   (cons
-                    "Switch to eshell"
-                    (lambda (candidate)
-                      (if (bufferp candidate)
-                          (switch-to-buffer candidate)
-                        (let ((default-directory candidate))
-                          (eshell t))))))
-          ;; make the candidates get re-generated on input, so one can
-          ;; actually create an eshell in a new directory
-          :volatile t)
-        :preselect
-        (->> (cogent/eshell-buffers)
-            first
-            (buffer-local-value 'default-directory)
-            pwd-replace-home
-            regexp-quote
-            (concat "^"))
-        :buffer "*helm eshell*"
-        :prompt "eshell in: "))
+  (add-hook 'helm-after-update-hook
+            #'cogent/eshell-helm-move-to-first-real-candidate)
+  (unwind-protect
+      (helm :sources
+            (helm-build-sync-source "eshell"
+              :candidates #'cogent/eshell-helm--get-candidates
+              :action (list
+                       (cons
+                        "Switch to eshell"
+                        (lambda (candidate)
+                          (if (bufferp candidate)
+                              (switch-to-buffer candidate)
+                            (let ((default-directory candidate))
+                              (eshell t))))))
+              ;; make the candidates get re-generated on input, so one can
+              ;; actually create an eshell in a new directory
+              :volatile t
+              :cleanup
+              (lambda ()
+                (remove-hook 'helm-after-update-hook
+                             #'cogent/eshell-helm-move-to-first-real-candidate)) )
+            :buffer "*helm eshell*"
+            :prompt "eshell in: ")))
 
 (provide 'cogent-eshell-helm)
