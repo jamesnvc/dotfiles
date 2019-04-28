615edae22999e8b61eb59375091fb94278eb7dcd
James N. V. Cash <james.nvc@gmail.com>
2019-04-28 10:47:03 -0400

    Make helm eshell switcher thing work

--- emacs.d/modules/cogent-eshell-helm.el
+++ emacs.d/modules/cogent-eshell-helm.el
@@ -8,24 +8,28 @@
 (defun cogent/eshell-helm ()
   "Switch between or create eshell buffers using helm"
   (interactive)
-  (helm :sources (helm-build-sync-source "eshell"
-                   :candidates (lambda ()
-                                 (->> (buffer-list)
-                                     (mapcar
-                                      (lambda (buff)
-                                        (when (string-prefix-p "*eshell*" (buffer-name buff))
-                                          (let ((dir (buffer-local-value 'default-directory buff)))
-                                            (cons (pwd-replace-home dir) buff)))))
-                                     (remove nil)))
-                   ;; [TODO] make completing-read function be directory
-                   :action (lambda (candidate)
-                             (message "candidate %s" candidate)
-                             ;; [TODO] need to make helm allow candidate that wasn't in the list
-                             (if (bufferp candidate)
-                                 (switch-to-buffer candidate)
-                               (let ((default-directory candidate))
-                                 (eshell t)))))
-        :input default-directory
+  (helm :sources
+        (helm-build-sync-source "eshell"
+          :candidates (lambda ()
+                        (->> (buffer-list)
+                            (mapcar
+                             (lambda (buff)
+                               (when (string-prefix-p "*eshell*" (buffer-name buff))
+                                 (let ((dir (buffer-local-value 'default-directory buff)))
+                                   (cons (pwd-replace-home dir) buff)))))
+                            (cons
+                             (let ((new-dir (if (string-blank-p helm-input) default-directory helm-input)))
+                               (cons (s-concat "new " (pwd-replace-home new-dir))
+                                     new-dir)))
+                            (remove nil)))
+          ;; [TODO] make completing-read function be directory
+          :action (lambda (candidate)
+                    (message "candidate %s" candidate)
+                    (if (bufferp candidate)
+                        (switch-to-buffer candidate)
+                      (let ((default-directory candidate))
+                        (eshell t))))
+          :volatile t)
         :buffer "*helm eshell*"))
 
 (provide 'cogent-eshell-helm)
--- emacs.d/user.el
+++ emacs.d/user.el
@@ -27,7 +27,7 @@
 (set-register ?e (cons 'file (concat dotfiles-dir "user.el")))
 
 (general-define-key :keymaps 'global
-                    "<f3>" #'eshell
+                    "<f3>" #'cogent/eshell-helm
                     "<f4>" #'calc
                     "<f5>" #'notmuch
                     "<f6>" #'elfeed
