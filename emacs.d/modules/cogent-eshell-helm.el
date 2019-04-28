ade9a4afed23eb14858d232a2e8174140fa3d60d
James N. V. Cash <james.nvc@gmail.com>
2019-04-28 14:40:12 -0400

    Make eshell helm switcher default to first shell

--- emacs.d/modules/cogent-eshell-helm.el
+++ emacs.d/modules/cogent-eshell-helm.el
@@ -4,6 +4,11 @@
 (require 'helm-lib)
 (require 'cl-lib)
 
+(defun cogent/eshell-buffers ()
+  (cl-loop for buf in (buffer-list)
+           when (string-prefix-p "*eshell*" (buffer-name buf))
+           collect buf))
+
 ;;;###autoload
 (defun cogent/eshell-helm ()
   "Switch between or create eshell buffers using helm"
@@ -12,19 +17,24 @@
         (helm-build-sync-source "eshell"
           :candidates
           (lambda ()
-            (-> (cl-loop for buf in (buffer-list)
-                     when (string-prefix-p "*eshell*" (buffer-name buf))
-                     collect (cons (pwd-replace-home
-                                    (buffer-local-value
-                                     'default-directory buf))
-                                   buf))
-                (append
-                 (let ((new-dir (if (string-blank-p helm-input)
-                                    default-directory
-                                  helm-input)))
-                   (list
-                    (cons (s-concat "new " (pwd-replace-home new-dir))
-                         new-dir))))))
+            (->>
+             (cl-loop for buf in (cogent/eshell-buffers)
+                      collect (cons (pwd-replace-home
+                                     (buffer-local-value
+                                      'default-directory buf))
+                                    buf))
+             (cons
+              (let ((new-dir (if (string-blank-p helm-input)
+                                 default-directory
+                               helm-input)))
+                (cons (concat
+                       (propertize
+                        " " 'display
+                        (propertize "[+]" 'font-lock-face
+                                    '(:background "#ff69c6" :foreground "#282a36")))
+                       " "
+                       (pwd-replace-home new-dir))
+                      new-dir)))))
           :action (list
                    (cons
                     "Switch to eshell"
@@ -36,6 +46,14 @@
           ;; make the candidates get re-generated on input, so one can
           ;; actually create an eshell in a new directory
           :volatile t)
-        :buffer "*helm eshell*"))
+        :preselect
+        (->> (cogent/eshell-buffers)
+            first
+            (buffer-local-value 'default-directory)
+            pwd-replace-home
+            regexp-quote
+            (concat "^"))
+        :buffer "*helm eshell*"
+        :prompt "eshell in: "))
 
 (provide 'cogent-eshell-helm)
