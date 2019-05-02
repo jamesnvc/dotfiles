9b93845d26a9e838e79a5db464cf265a3c751ab0
James N. V. Cash <james.nvc@gmail.com>
2019-05-02 18:12:13 -0400

    Add disk-usage package

--- emacs.d/modules/cogent-os.el
+++ emacs.d/modules/cogent-os.el
@@ -3,4 +3,9 @@
 (use-package helm-system-packages
   :commands helm-system-packages)
 
+(use-package disk-usage
+  :commands (disk-usage disk-usage-here)
+  :config
+  (evil-set-initial-state 'disk-usage-mode 'emacs))
+
 (provide 'cogent-os)
