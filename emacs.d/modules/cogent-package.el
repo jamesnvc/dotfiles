34c285b5a7d3d64de3de463f1cf096168323bbff
James N. V. Cash <james.nvc@gmail.com>
2019-05-03 12:47:51 -0400

    Formatting

--- emacs.d/modules/cogent-package.el
+++ emacs.d/modules/cogent-package.el
@@ -18,9 +18,10 @@
                                         user-emacs-directory))
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
-    (with-current-buffer (url-retrieve-synchronously
-                          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-                          'silent 'inhibit-cookies)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
