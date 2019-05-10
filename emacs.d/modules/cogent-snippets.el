a6c21f01047f76dd11908d61fd5815befca81fbc
James N. V. Cash <james.nvc@gmail.com>
2019-05-10 14:57:24 -0400

    Include yasnippets as company backend
    There's also a trick where we modify all the backends to be like
    '(company-thing . (:with company-yasnippet)), but I'm happy with it
    yasnippet just coming up if I keep hitting C-\

--- emacs.d/modules/cogent-snippets.el
+++ emacs.d/modules/cogent-snippets.el
@@ -2,6 +2,9 @@
 
 (use-package yasnippet
   :demand t
-  :config (yas-global-mode 1))
+  :config
+  (yas-global-mode 1)
+  (with-eval-after-load 'company
+    (add-to-list 'company-backends #'company-yasnippet t)))
 
 (provide 'cogent-snippets)
