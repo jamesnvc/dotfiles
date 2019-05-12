7af9161b45943b861042fc5a83b79c59037ac859
James N. V. Cash <james.nvc@gmail.com>
2019-05-11 20:26:23 -0400

    Make emacs misc langs stuff deferred

--- emacs.d/modules/cogent-misc-langs.el
+++ emacs.d/modules/cogent-misc-langs.el
@@ -1,14 +1,18 @@
 ;;; -*- lexical-binding: t -*-
 (require 'cogent-package)
 
-(use-package swift-mode)
+(use-package swift-mode
+  :defer t)
 
-(use-package haskell-mode)
+(use-package haskell-mode
+  :defer t)
 
-(use-package lua-mode)
+(use-package lua-mode
+  :defer t)
 
 ;;(use-package racket-mode)
 
-(use-package yaml-mode)
+(use-package yaml-mode
+  :defer t)
 
 (provide 'cogent-misc-langs)
