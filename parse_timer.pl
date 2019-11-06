4768bf82f6fd2c9e8d95c093f0b712f4659a51cf
James N. V. Cash <james.nvc@gmail.com>
2019-11-05 19:53:03 -0500

    Make timer script for bar show left/passed

--- parse_timer.pl
+++ parse_timer.pl
@@ -5,4 +5,14 @@ my $headers = <>;
 my $start = index($headers, "LEFT");
 my $end = index($headers, "LAST");
 my $line = <>;
-print substr($line, $start, $end - $start);
+my $left = substr($line, $start, $end - $start);
+$left =~ s/^\s+//;
+$left =~ s/\s+$//;
+if ($left ne "n/a") {
+  print($left);
+} else {
+  my $passed_start = index($headers, "PASSED");
+  my $passed_end = index($headers, "UNIT");
+  my $passed = substr($line, $passed_start, $passed_end - $passed_start);
+  print($passed);
+}
