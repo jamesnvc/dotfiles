e9e3f65301cfe738a9be8e4d13e2eabb9ffce321
James N. V. Cash <james.nvc@gmail.com>
2019-12-24 12:51:09 -0500

    Add direnv hook for fish, fix LD_PRELOAD on Ubuntu

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,14 @@ function fish_default_mode_prompt
 end
 
 set -gx EDITOR 'emacsclient --create-frame --alternate-editor=emacs'
-
+# overriding weird ubuntu thing
+if test ( uname ) = 'Linux'
+    set -gx LD_PRELOAD /usr/lib/x86_64-linux-gnu/libgtk3-nocsd.so.0
+end
 # set -U fish_key_bindings fish_vi_key_bindings
 
 alias g=git
 alias l=ls
 alias ec="emacsclient --create-frame --alternate-editor=emacs"
+
+direnv hook fish | source
