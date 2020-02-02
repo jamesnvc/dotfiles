93fc8dae52077a06af9e8fccdf57faa86f41ea84
James N. V. Cash <james.nvc@gmail.com>
2020-02-02 16:52:53 -0500

    Defer loading pdf-tools

--- emacs.d/modules/cogent-reading.el
+++ emacs.d/modules/cogent-reading.el
@@ -3,6 +3,7 @@
 (require 'cogent-package)
 
 (use-package pdf-tools
+  :defer t
   :commands (pdf-view-mode pdf-tools-install)
   :mode ("\\.[pP][dD][fF]\\'" . pdf-view-mode)
   :magic ("%PDF" . pdf-view-mode)
