e648c9ca3201000d25acebedad9c6409a34cc97d
James N. V. Cash <james.nvc@gmail.com>
2020-10-01 11:13:21 -0400

    Use local `dict` to lookup definitions if available

--- emacs.d/modules/cogent-writing.el
+++ emacs.d/modules/cogent-writing.el
@@ -6,18 +6,24 @@
   (interactive (list (if (region-active-p)
                          (buffer-substring-no-properties (region-beginning) (region-end))
                        (save-excursion (car (ispell-get-word nil))))))
-  (url-retrieve
-   (format "https://en.wiktionary.org/wiki/%s" word)
-   (lambda (status)
-     (let ((content (->
-                     (libxml-parse-html-region (point-min) (point-max))
-                     (dom-by-id "mw-content-text")))
-           (buf (get-buffer-create "*Help Definition*")))
-       (with-current-buffer buf
-         (delete-region (point-min) (point-max))
-         (insert (dom-texts (car (dom-by-tag content 'p))))
-         (insert "\n\n")
-         (insert (dom-texts (car (dom-by-tag content 'ol)))))
-       (switch-to-buffer-other-window buf)))))
+  (if-let ((dict (executable-find "dict")))
+      (let ((buf (get-buffer-create "*Help Definition*")))
+        (with-current-buffer buf (delete-region (point-min) (point-max)))
+        ;; [TODO] allow chosing other dictionaries
+        (call-process dict nil buf nil word)
+        (switch-to-buffer-other-window buf))
+    (url-retrieve
+     (format "https://en.wiktionary.org/wiki/%s" word)
+     (lambda (status)
+       (let ((content (->
+                       (libxml-parse-html-region (point-min) (point-max))
+                       (dom-by-id "mw-content-text")))
+             (buf (get-buffer-create "*Help Definition*")))
+         (with-current-buffer buf
+           (delete-region (point-min) (point-max))
+           (insert (dom-texts (car (dom-by-tag content 'p))))
+           (insert "\n\n")
+           (insert (dom-texts (car (dom-by-tag content 'ol)))))
+         (switch-to-buffer-other-window buf))))))
 
 (provide 'cogent-writing)
