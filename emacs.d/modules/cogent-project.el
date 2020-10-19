a6b48bec39aa9a84ff7ce61f447b46e23aa9c756
James N. V. Cash <james.nvc@gmail.com>
2020-10-19 11:41:46 -0400

    Remove unused ibuffer-projectile package

--- emacs.d/modules/cogent-project.el
+++ emacs.d/modules/cogent-project.el
@@ -7,13 +7,6 @@
   :defer 1
   :bind ("C-c C-f" . projectile-find-file))
 
-(use-package ibuffer-projectile
-  :bind ("C-x C-b" . ibuffer)
-  :hook (ibuffer-hook . (lambda ()
-                          (ibuffer-projectile-set-filter-groups)
-                          (unless (eq ibuffer-sorting-mode 'alphabetic)
-                            (ibuffer-do-sort-by-alphabetic)))))
-
 (use-package dumb-jump
   :hook (xref-backend-functions . dumb-jump-xref-activate)
   :custom ((dumb-jump-selector 'helm)))
