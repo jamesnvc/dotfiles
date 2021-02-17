dccd844fbebf292fe89c9136ea28e78486873435
James N. V. Cash <james.nvc@gmail.com>
2021-02-17 07:59:18 -0500

    Use js mode instead of js2-mode

--- emacs.d/modules/cogent-javascript.el
+++ emacs.d/modules/cogent-javascript.el
@@ -4,37 +4,11 @@
 (require 'cogent-base)
 (require 'cogent-json)
 
-(use-package js2-mode
-  :mode (("\\.js$" . js2-mode)
-         ("\\.es6\\'" . js2-mode)
-         ("\\.ejs\\'" . js2-mode))
-  :interpreter "node"
-  :commands js2-mode
-  :config
-  ;; If npm is installed, add its local prefix to search path, to help find linters
-  (when-let ((npm-prefix (cogent/exec-if-exec "npm" "config get prefix")))
-    (setenv "PATH" (concat npm-prefix "/bin:" (getenv "PATH")))
-    (add-to-list 'exec-path (concat npm-prefix "/bin")))
-
-  (setq-default
-   js2-mode-indent-ignore-first-tab t
-   js2-strict-inconsistent-return-warning nil
-   js2-global-externs '("module" "require" "__dirname" "process" "console"
-                        "JSON" "$" "_")))
-
 (use-package rjsx-mode
   :defer t)
 
-(use-package js2-refactor
-  :after js2-mode
-  :commands (js2r-add-keybindings-with-prefix)
-  :hook (js2-mode-hook . js2-refactor-mode)
-  :init
-  (js2r-add-keybindings-with-prefix "C-c C-m"))
-
 (use-package tern
   :commands tern-mode
-  :hook (js2-mode-hook . (lambda () (tern-mode t)))
   :config
   (setq tern-command (list (or (executable-find "tern") "tern"))))
 
@@ -117,13 +91,13 @@
            ((derived-mode-p 'sass-mode) 'autoformat-sass-command)
            ;; JS projects with eslint config
            ((and (file-exists-p eslint-path)
-                 (derived-mode-p 'js2-mode))
+                 (derived-mode-p 'js-mode))
             'autoformat-prettier-eslint-command)
-           ((derived-mode-p 'js2-mode) 'autoformat-javascript-command)))))
+           ((derived-mode-p 'js-mode) 'autoformat-javascript-command)))))
 
 (general-def
   :states '(normal)
-  :keymaps '(js2-mode-map css-mode-map)
+  :keymaps '(js-mode-map css-mode-map)
   "<SPC> f" #'autoformat)
 
 (provide 'cogent-javascript)
