e2e1c4510bca5fb5c61b3a3df1cca514f3093bd5
James N. V. Cash <james.nvc@gmail.com>
2021-05-25 09:52:35 -0400

    Add emacs modes for scala & J

--- emacs.d/modules/cogent-misc-langs.el
+++ emacs.d/modules/cogent-misc-langs.el
@@ -15,4 +15,20 @@
 (use-package yaml-mode
   :defer t)
 
+(use-package scala-mode
+  :defer t
+  :mode ("\\.scala\\'" . scala-mode)
+  :commands scala-mode
+  :interpreter
+  ("scala" . scala-mode))
+
+(use-package j-mode
+  :defer t
+  :straight (j-mode
+             :type git
+             :host github
+             :repo "zellio/j-mode")
+  :config
+  (setq j-console-cmd "ijconsole"))
+
 (provide 'cogent-misc-langs)
