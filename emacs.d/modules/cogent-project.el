5141092424ef9e9aad578e6eaa7f19cd57459f4c
James N. V. Cash <james.nvc@gmail.com>
2021-07-17 19:40:15 -0400

    Project bindings to match projectile helm ones

--- emacs.d/modules/cogent-project.el
+++ emacs.d/modules/cogent-project.el
@@ -6,4 +6,12 @@
   :hook (xref-backend-functions . dumb-jump-xref-activate)
   :custom ((dumb-jump-selector 'completing-read)))
 
+(use-package project
+  :straight (:type built-in)
+  :config
+  (cogent/leader-def
+    :states '(normal visual)
+    "P" #'project-switch-project
+    "t" #'project-find-file))
+
 (provide 'cogent-project)
