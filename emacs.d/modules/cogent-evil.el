a65c3872edc95abb446717454e59deb107fa4c6b
James N. V. Cash <james.nvc@gmail.com>
2021-07-22 12:00:23 -0400

    More modes to start in emacs state

--- emacs.d/modules/cogent-evil.el
+++ emacs.d/modules/cogent-evil.el
@@ -21,6 +21,7 @@
   (evil-set-initial-state 'man-mode 'emacs)
   (evil-set-initial-state 'vundo--mode 'emacs)
   (evil-set-initial-state 'completion-mode 'emacs)
+  (evil-set-initial-state 'process-menu-mode 'emacs)
 
   (defun cogent/evil-yank-to-eol (&optional argument)
     "Yank from point to end of line; like the behaviour I prefer `Y' in
