91f47db606802d7e5d27cc78c3bdb6cfeaf2113c
James N. V. Cash <james.nvc@gmail.com>
2021-11-16 16:40:26 -0500

    Fixing(?) completion in lsp-mode

--- emacs.d/modules/cogent-complete.el
+++ emacs.d/modules/cogent-complete.el
@@ -7,6 +7,13 @@
   (corfu-global-mode)
   :custom
   (corfu-quit-no-match t)
+  :config
+  (with-eval-after-load 'lsp
+    (setq lsp-completion-provider :none)
+    (defun cogent/corfu-lsp-setup ()
+      (setq-local completion-styles '(orderless)
+                  completion-category-defaults nil))
+    (add-hook 'lsp-mode-hook #'cogent/corfu-lsp-setup))
   :general
   (:keymaps 'corfu-map :states 'insert
             [escape] #'corfu-quit)
