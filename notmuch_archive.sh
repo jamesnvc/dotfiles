1583796c128e608bf40f8302fa3b4af2a19badf7
James N. V. Cash <james.nvc@gmail.com>
2022-05-06 08:07:24 -0400

    Delete mail from inbox if `mv -n` didn't move it

--- notmuch_archive.sh
+++ notmuch_archive.sh
@@ -8,6 +8,7 @@ while IFS= read -r -d $'\0' path; do
     file=$(basename "${path}")
     esc_file=${file//,U=[0-9]*([0-9])/}
     mv -n "${path}" "${HOME}/.mail/fastmail/Archive/cur/${esc_file}"
+    rm -f "${path}"
 done < <(notmuch search --output=files --format=text0 -- folder:Inbox and not tag:inbox)
 
 notmuch new
