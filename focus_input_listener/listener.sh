00121464bc70e93a20b248de776552efcc69b6b7
James N. V. Cash <james.nvc@gmail.com>
2022-06-01 20:18:24 -0400

    Focus listener to input unicode characters on macOS
    Since the unicode input from the keyboard directly is awkward (it
    requires a specific input method and doesn't work well with emoji).
    Instead, the keyboardio now sends Focus events over the serial port
    with the keycode; this little script, started as a launchagent,
    listens, generates the corresponding character, and pastes it in via
    system events.
    
    A bit of a Rube Goldberg machine, but it seems to work...

---
+++ focus_input_listener/listener.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+# shellcheck disable=SC3000-SC4000
+
+set -euo pipefail
+
+input_codepoint(){
+    local char=$(perl -C -e "print chr ${1}")
+    osascript <<-EOF
+    tell application "System Events"
+      set tmp to the clipboard
+      set the clipboard to "${char}"
+      keystroke "v" using command down
+      delay 0.2
+      set the clipboard to tmp
+    end tell
+EOF
+}
+
+listen() {
+    SERIAL_DEVICES=(/dev/cu.usbmodem*)
+    SERIAL_DEVICE="${SERIAL_DEVICES[0]}"
+
+    echo "Listening on ${SERIAL_DEVICE}" >&2
+
+    while IFS= read -r -d $'\n' line
+    do
+        IFS=' ' read -ra command <<< "$line"
+        case "${command[0]}" in
+            unicode_input)
+                input_codepoint "${command[1]}";;
+            *) echo "Unknown event ${command[0]}";;
+       esac
+    done < "${SERIAL_DEVICE}"
+}
+
+listen
