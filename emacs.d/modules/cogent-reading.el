ee7ff4cc7d7e1723e5c98df340d986ccc9280abb
James N. V. Cash <james.nvc@gmail.com>
2022-06-03 07:59:52 -0400

    Little "bionic reading" lisp snippet

--- emacs.d/modules/cogent-reading.el
+++ emacs.d/modules/cogent-reading.el
@@ -28,4 +28,29 @@
   (evil-set-initial-state 'nov-mode 'emacs)
   :mode ("\\.epub\\'" . nov-mode))
 
+(defvar infu-bionic-reading-face nil "a face for `infu-bionic-reading-region'.")
+
+(setq infu-bionic-reading-face 'bold)
+
+(defun infu-bionic-reading-buffer ()
+  "Bold the first few chars of every word in current buffer.
+Version 2022-05-21"
+  (interactive)
+  (infu-bionic-reading-region (point-min) (point-max)))
+
+(defun infu-bionic-reading-region (begin end)
+  "Bold the first few chars of every word in region.
+Version 2022-05-21"
+  (interactive "r")
+  (save-restriction
+    (narrow-to-region begin end)
+    (goto-char (point-min))
+    (while (forward-word)
+      (pcase-let ((`(,word-begin . ,word-end) (bounds-of-thing-at-point 'word)))
+        ;; bold the first half of the word to the left of cursor
+        (put-text-property
+         word-begin
+         (+ word-begin (1+ (/ (- word-end word-begin) 2)))
+         'font-lock-face infu-bionic-reading-face)))))
+
 (provide 'cogent-reading)
