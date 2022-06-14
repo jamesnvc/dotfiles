e744f0c33618266dd4e0294587f089dc527601b2
James N. V. Cash <james.nvc@gmail.com>
2022-06-13 20:46:06 -0400

    Use emacs built-in dictionary search if available

--- emacs.d/modules/cogent-writing.el
+++ emacs.d/modules/cogent-writing.el
@@ -6,17 +6,20 @@
   (interactive (list (if (region-active-p)
                          (buffer-substring-no-properties (region-beginning) (region-end))
                        (save-excursion (car (ispell-get-word nil))))))
-  (if-let ((dict (executable-find "dict")))
-      (let ((buf (get-buffer-create "*Help Definition*")))
-        (with-current-buffer buf (delete-region (point-min) (point-max)))
-        ;; [TODO] allow chosing other dictionaries
-        (call-process dict nil buf nil word)
-        (display-buffer buf 'display-buffer-pop-up-window))
+  (cond
+   ((fboundp 'dictionary-search) (dictionary-search word))
+   ((dict (executable-find "dict"))
+    (let ((buf (get-buffer-create "*Help Definition*")))
+      (with-current-buffer buf (delete-region (point-min) (point-max)))
+      ;; [TODO] allow chosing other dictionaries
+      (call-process dict nil buf nil word)
+      (display-buffer buf 'display-buffer-pop-up-window)))
+   (t
     (url-retrieve
      (format "https://en.wiktionary.org/wiki/%s" word)
      (lambda (_status)
        (let ((content (thread-first
-                          (libxml-parse-html-region (point-min) (point-max))
+                        (libxml-parse-html-region (point-min) (point-max))
                         (dom-by-id "mw-content-text")))
              (buf (get-buffer-create "*Help Definition*")))
          (with-current-buffer buf
@@ -24,6 +27,6 @@
            (insert (dom-texts (car (dom-by-tag content 'p))))
            (insert "\n\n")
            (insert (dom-texts (car (dom-by-tag content 'ol)))))
-         (display-buffer buf 'display-buffer-popup-window))))))
+         (display-buffer buf 'display-buffer-popup-window)))))))
 
 (provide 'cogent-writing)
