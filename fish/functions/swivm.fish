78d65e0fa16b58d7b31e4212ba0d656066be9d93
James N. V. Cash <james.nvc@gmail.com>
2022-06-13 20:09:35 -0400

    Making swivm work properly on macos with homebrew libs

--- fish/functions/swivm.fish
+++ fish/functions/swivm.fish
@@ -1,3 +1,3 @@
 function swivm
-    bash -c "source ~/.swivm/swivm.sh && swivm $argv"
+    bash -c "source ~/.swivm/swivm.sh && LDFLAGS=\"-L/opt/homebrew/opt/libarchive/lib\" CPPFLAGS=\"-I/opt/homebrew/opt/libarchive/include\" OPENSSL_ROOT_DIR=\"/opt/homebrew/opt/openssl@1.1\" swivm $argv"
 end
