c646d0b234bfb3f6dfd1994e4921ce9752846040
James N. V. Cash <james.nvc@gmail.com>
2022-11-06 19:38:20 -0500

    Add explanatory comment

--- emacs.d/modules/cogent-lsp.el
+++ emacs.d/modules/cogent-lsp.el
@@ -12,7 +12,7 @@
 
   (setq read-process-output-max (* 1024 1024))
   (setopt lsp-idle-delay 1.0)
-  (setopt lsp-completion-provider :none)
+  (setopt lsp-completion-provider :none) ; don't want company, using built-in completion now
   (setopt lsp-enable-snippet nil)
 
   ;; (add-to-list 'lsp-semantic-token-faces
