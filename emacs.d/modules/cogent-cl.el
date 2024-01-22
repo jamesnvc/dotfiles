60ba629892e5fc54bafe0b65ec990fe8ec04167b
James N. V. Cash <james.cash@occasionallycogent.com>
2024-01-22 18:02:45 -0500

    Common Lisp config

---
+++ emacs.d/modules/cogent-cl.el
@@ -0,0 +1,9 @@
+;;; cogent-cl.el --- Common Lisp config              -*- lexical-binding: t; -*-
+
+(use-package slime
+  :config
+  (setopt inferior-lisp-program "sbcl"))
+
+
+(provide 'cogent-cl)
+;;; cogent-cl.el ends here
