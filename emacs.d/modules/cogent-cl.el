fdaa54402ebd228fd9ebf820daebfc3c3026fbfe
James N. V. Cash <james.cash@occasionallycogent.com>
2024-01-24 15:39:34 -0500

    Make evaling lisp in docstrings work properly

--- emacs.d/modules/cogent-cl.el
+++ emacs.d/modules/cogent-cl.el
@@ -7,26 +7,34 @@
   (setopt inferior-lisp-program (executable-find "sbcl"))
   (setopt sly-symbol-completion-mode nil)
 
+  (defun cogent-lisp/form-in-region (beg end)
+    "Get the lisp code to evaluate in region, unescaping strings if necessary."
+    (let* ((ppss (syntax-ppss))
+           (in-string-p (nth 3 ppss))
+           (string (buffer-substring-no-properties beg end)))
+      (if in-string-p
+          (string-replace "\\" "" string)
+        string)))
+
   (evil-define-operator cogent/evil-sly-eval (beg end)
     "Evaluate Common Lisp expression given by <motion> via sly."
-    (sly-eval-region beg end))
+    (sly-eval-with-transcript
+     `(slynk:interactive-eval-region ,(cogent-lisp/form-in-region beg end))))
+
   (evil-define-operator cogent/evil-sly-eval-in-place (beg end)
     "Evaluate Common Lisp expression given by <motion> via sly and
 insert the result."
-    (sly-eval-async `(slynk:eval-and-grab-output ,(buffer-substring-no-properties beg end))
+    (sly-eval-async `(slynk:eval-and-grab-output
+                      ,(cogent-lisp/form-in-region beg end))
       (lambda (result)
         (cl-destructuring-bind (output value) result
-          (push-mark)
-          (let* ((start end)
-                 (ppss (syntax-ppss))
+          (let* ((ppss (syntax-ppss))
                  (string-or-comment-p (or (nth 3 ppss) (nth 4 ppss))))
             (save-excursion
               (goto-char end)
-              (insert output (if string-or-comment-p
-                                 ""
-                               " => ") value)
+              (insert output " => " value)
               (unless string-or-comment-p
-                (comment-region start (point) 1))))))))
+                (comment-region end (point) 1))))))))
 
   (general-nmap 'sly-mode-map
     "go" 'cogent/evil-sly-eval
