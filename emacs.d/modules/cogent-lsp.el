22e8365948b88169be0cde3b8176564badbeee38
James N. V. Cash <james.cash@occasionallycogent.com>
2025-04-23 20:26:24 -0400

    Bind key to eglot-format instead of eglot-format-buffer
    So it will act on region if active

--- emacs.d/modules/cogent-lsp.el
+++ emacs.d/modules/cogent-lsp.el
@@ -17,7 +17,7 @@
                                  eglot-server-programs))
   (keymap-set eglot-mode-map "C-c C-a" #'eglot-code-actions)
   (keymap-set eglot-mode-map "C-c C-r" #'eglot-rename)
-  (keymap-set eglot-mode-map "C-c C-f" #'eglot-format-buffer))
+  (keymap-set eglot-mode-map "C-c C-f" #'eglot-format))
 
 (use-package flymake
   :straight (:type built-in)
