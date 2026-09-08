d93a2152b497cef1d195375f5c1b6bdcb72e93ce
James N. V. Cash <james.cash@occasionallycogent.com>
2026-09-08 11:04:04 -0400

    Force more secure defauts for gnutls

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,4 +1,6 @@
 ;; -*- lexical-binding: t; -*-
+(setq gnutls-verify-error t)
+(setq gnutls-min-prime-bits 3072)
 (setq-default mode-line-format nil)
 (setq package-enable-at-startup nil)
 (setq package-quickstart nil)
