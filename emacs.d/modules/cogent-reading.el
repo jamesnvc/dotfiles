5dbbc0ba67ad4ff8c73a22aabd4b528450b1d38e
James N. V. Cash <james.cash@occasionallycogent.com>
2026-09-17 13:44:30 -0400

    Get rid of old viewer libraries

--- emacs.d/modules/cogent-reading.el
+++ emacs.d/modules/cogent-reading.el
@@ -2,34 +2,6 @@
 
 (require 'cogent-package)
 
-(comment
- (use-package pdf-tools
-  :defer t
-  :straight (pdf-tools
-             :type git
-             :host github
-             :repo "vedang/pdf-tools")
-  :commands (pdf-view-mode pdf-tools-install)
-  :mode ("\\.[pP][dD][fF]\\'" . pdf-view-mode)
-  :magic ("%PDF" . pdf-view-mode)
-  :config
-  (pdf-tools-install)
-  (define-pdf-cache-function pagelabels)
-  :hook ((pdf-view-mode-hook . (lambda () (display-line-numbers-mode -1)))
-         (pdf-view-mode-hook . pdf-tools-enable-minor-modes))))
-
-(use-package org-pdftools
-  :defer t
-  :hook (org-load-hook . org-pdftools-setup-link))
-
-(comment
- (use-package nov
-  :defer t
-  :commands nov-mode
-  :config
-  (evil-set-initial-state 'nov-mode 'emacs)
-  :mode ("\\.epub\\'" . nov-mode)))
-
 (use-package reader
   :straight '(reader :type git
                      :host codeberg
@@ -42,30 +14,5 @@
   (add-to-list 'magic-mode-alist '("%PDF" . reader-mode))
   (evil-set-initial-state 'reader-mode 'emacs)
   (add-hook 'reader-mode-hook (lambda () (display-line-numbers-mode -1))))
-
-(defvar infu-bionic-reading-face nil "a face for `infu-bionic-reading-region'.")
-
-(setq infu-bionic-reading-face 'bold)
-
-(defun infu-bionic-reading-buffer ()
-  "Bold the first few chars of every word in current buffer.
-Version 2022-05-21"
-  (interactive)
-  (infu-bionic-reading-region (point-min) (point-max)))
-
-(defun infu-bionic-reading-region (begin end)
-  "Bold the first few chars of every word in region.
-Version 2022-05-21"
-  (interactive "r")
-  (save-restriction
-    (narrow-to-region begin end)
-    (goto-char (point-min))
-    (while (forward-word)
-      (pcase-let ((`(,word-begin . ,word-end) (bounds-of-thing-at-point 'word)))
-        ;; bold the first half of the word to the left of cursor
-        (put-text-property
-         word-begin
-         (+ word-begin (1+ (/ (- word-end word-begin) 2)))
-         'font-lock-face infu-bionic-reading-face)))))
 
 (provide 'cogent-reading)
